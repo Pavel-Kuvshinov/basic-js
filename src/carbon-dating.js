491bd9a0a6a7a0a636cd42c3b6d3d3ed27df00d3
Pavel Kuvshinov <pavelkuvsh1noff@mail.ru>
2024-12-13 20:36:25 +0300

    feat: End carbon-dating subtask.

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,11 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if(typeof sampleActivity !== "string" || sampleActivity.length == 0 || isNaN(sampleActivity)) return false;
+  if (sampleActivity > 15 || sampleActivity <= 0) return false;
+
+  return Math.ceil((Math.log(MODERN_ACTIVITY/sampleActivity))/(Math.LN2/HALF_LIFE_PERIOD));
 }
 
 module.exports = {
