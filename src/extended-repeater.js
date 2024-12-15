b2273aac337d0b4226abaf91ef4b572709db3105
Pavel Kuvshinov <pavelkuvsh1noff@mail.ru>
2024-12-15 18:37:02 +0300

    feat: Finished extended-repeater subtask.

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  const STR = String(str);
+  const DEFAULT_SEPARATOR = "+";
+  const DEFAULT_ADDITIONAL_SEPARATOR = "|";
+
+  let memSeparator = DEFAULT_SEPARATOR;
+  let memAdditionalSeparator = DEFAULT_ADDITIONAL_SEPARATOR;
+  let memStr = STR;
+  let resStr = '';
+
+  if(options.separator) memSeparator = options.separator;
+  if(options.additionSeparator) memAdditionalSeparator = options.additionSeparator;
+
+  if(options.addition !== undefined){
+    for(let i = 0; i < options.additionRepeatTimes - 1; i += 1) {
+      memStr += String(options.addition) + memAdditionalSeparator;
+    }
+    memStr += String(options.addition);
+  }
+
+  resStr = memStr;
+
+  for (let i = 0; i < options.repeatTimes - 1; i += 1) {
+    resStr += memSeparator + memStr;
+  }
+
+  return resStr;
 }
 
 module.exports = {
