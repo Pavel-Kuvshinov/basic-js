96c8870d4851c2a6c1082ef11812560aa5fa653f
Pavel Kuvshinov <pavelkuvsh1noff@mail.ru>
2024-12-16 21:53:19 +0300

    feat: Finished simple-chain subtask

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  memChain: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.memChain.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    if (value === undefined) value = '';
+    this.memChain.push(`( ${value} )`);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    let memChainPos = position - 1;
+    if(!this.memChain[memChainPos]){
+      this.memChain.splice(0);
+      throw new Error ("You can\'t remove incorrect link!");
+    }
+    this.memChain.splice(memChainPos, 1);
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.memChain.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let finalChain = this.memChain.join('~~');
+    this.memChain.splice(0);
+    return finalChain;
   }
 };
 
